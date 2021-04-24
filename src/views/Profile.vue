b50e25bdc4d8a7959cb3e6bf5dd669a2aabc1041
mweiner01 <max.makkusuu@gmail.com>
2021-04-24 18:08:16 +0100

    added select

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -45,8 +45,8 @@
             <button class="py-1 px-2 bg-green-400 hover:bg-green-500 text-white rounded text-base focus:outline-none" @click="showFormular = !showFormular">Create Milestone</button>
         </div>
     </div>
-    <div class="py-12" v-if="showFormular">
-        <form @submit.prevent="add()" class="text-gray-700 max-w-xs mx-auto">
+    <div class="max-w-5xl py-12" v-if="showFormular">
+        <form @submit.prevent="add()" class="text-gray-700 max-w-2xl mx-auto">
             <div class="my-4">
                 <h1 class="text-green-500 text-xl font-semibold text-center font-light">Create Milestone</h1>
             </div>
@@ -54,11 +54,13 @@
                 <input v-model="form.title" class="bg-gray-200 py-2 pl-2 pr-6 w-full rounded border-2 border-transparent placeholder-gray-700 focus:border-green-400 focus:outline-none" type="text" placeholder="Title" required>
             </div>
             <div class="my-2">
-                <input v-model="form.type" class="bg-gray-200 py-2 pl-2 pr-6 w-full rounded border-2 border-transparent placeholder-gray-700 focus:border-green-400 focus:outline-none" type="text" placeholder="Type" required>
-            </div>
-            <div class="my-2">
                 <input v-model="form.hours" class="bg-gray-200 py-2 pl-2 pr-6 w-full rounded border-2 border-transparent placeholder-gray-700 focus:border-green-400 focus:outline-none" type="text" placeholder="Hours" required>
             </div>
+            <select v-model="form.type" class="bg-gray-200 py-2 pl-2 pr-6 w-full rounded border-2 border-transparent placeholder-gray-700 focus:border-green-400 focus:outline-none">
+                <option class="py-1">Reading</option>
+                <option class="py-1">Listening</option>
+                <option class="py-1">Passive Listening</option>
+            </select>
             <div class="my-2">
                 <button type="submit" class="py-2 px-12 bg-green-400 hover:bg-green-500 rounded text-white cursor-pointer w-full">Create Milestone</button>
             </div>
